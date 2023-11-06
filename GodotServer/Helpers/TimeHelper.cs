39a0a3a860c942ff43f0849edb538fc7ec82f2ca
Andrey Klyuev <klandrew@gmail.com>
2023-11-06 21:51:44 +0000

    Better ingame time and wip buffs

--- GodotServer/Helpers/TimeHelper.cs
+++ GodotServer/Helpers/TimeHelper.cs
@@ -5,13 +5,11 @@ namespace SphServer.Helpers
 
     public static class TimeHelper
     {
-        public const int UnixTimeOrigin = 1649722100;
+        public static readonly DateTime RealtimeOrigin = new (1998, 8, 21, 10, 00, 00);
 
         public static DateTime GetCurrentSphereDateTime()
         {
-            var now = new DateTimeOffset(DateTime.UtcNow);
-            var fromUnixTimeOrigin = now.ToUnixTimeSeconds() - UnixTimeOrigin;
-            var sphereTimeOffset = fromUnixTimeOrigin * 12;
+            var sphereTimeOffset = (DateTime.UtcNow - RealtimeOrigin).TotalSeconds * 12;
             var sphereDateTime = new DateTime().AddSeconds(sphereTimeOffset);
 
             return sphereDateTime;
@@ -20,10 +18,10 @@ namespace SphServer.Helpers
         public static byte[] EncodeCurrentSphereDateTime()
         {
             var currentSphereTime = GetCurrentSphereDateTime();
-            // ingame time shows hours and minutes, we'll ignore seconds
+            var seconds = currentSphereTime.Second / 12 + 1;
             var minutes_last4 = (byte)((currentSphereTime.Minute & 0b1111) << 4);
-            // 1-4 minutes 5-8 idk 
-            var firstDateByte = (byte)(minutes_last4 + 0b1000);
+            // 1-4 minutes 5-8 seconds 
+            var firstDateByte = (byte)(minutes_last4 + (seconds & 0b1111));
             var minutes_first2 = (byte)((currentSphereTime.Minute & 0b110000) >> 4);
             var hours = (byte)(currentSphereTime.Hour << 2);
             var days_last1 = (byte)((currentSphereTime.Day % 2) << 7);
@@ -34,14 +32,12 @@ namespace SphServer.Helpers
             // 1-4 months 5-8 days
             var thirdDateByte = (byte)(month + days_first4);
             var years_last8 = (byte)(currentSphereTime.Year & 0b11111111);
-            var years_first1 = (byte)((currentSphereTime.Year & 0b100000000) >> 8);
-            // quite likely it has more digits but 20+ irl years should be enough
-            var fourthDateByte = (byte)(0b00110100 + years_first1);
+            var years_first2 = (byte)((currentSphereTime.Year & 0b1100000000) >> 8);
+            var fourthDateByte = (byte)(0b00110100 + years_first2);
 
             return new[]
             {
-                firstDateByte, secondDateByte,
-                thirdDateByte, years_last8, fourthDateByte
+                firstDateByte, secondDateByte, thirdDateByte, years_last8, fourthDateByte
             };
         }
     }
