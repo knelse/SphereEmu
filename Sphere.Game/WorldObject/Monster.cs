646ae952433c03dae4cd68b546760f2c372bde65
Andrey Klyuev <klandrew@gmail.com>
2026-06-12 18:07:04 +0100

    Exposing mob data & updating on type change

--- Sphere.Game/WorldObject/Monster.cs
+++ Sphere.Game/WorldObject/Monster.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Godot;
 using SphServer.Packets;
 using SphServer.Shared.GameData.Enums;
+using SphServer.Sphere.Game;
 
 namespace SphServer.Sphere.Game.WorldObject;
 
@@ -33,6 +34,91 @@ public partial class Monster : WorldObject
 		return true;
 	}
 
+	private void RefreshMonsterInstanceFromType()
+	{
+		if (!MonsterTypeMapping.MonsterNameToMonsterTypeMapping.TryGetValue(_monsterType, out var monsterDbId))
+		{
+			return;
+		}
+
+		if (!GameObjectDb.Db.TryGetValue(monsterDbId, out var gameObject))
+		{
+			return;
+		}
+
+		MonsterInstance = new SphMonsterInstance(new SphMonsterData(gameObject), _level, _isNamed);
+		SyncExportedMonsterFields();
+		NotifyInspector();
+	}
+
+	private void RecalculateMonsterInstanceStatsFromLevel()
+	{
+		if (MonsterInstance is null)
+		{
+			return;
+		}
+
+		var data = MonsterInstance.MonsterDataOrigin;
+		var level = MonsterInstance.Level;
+		MonsterInstance.MaxHp = level * data.HpPerLevel;
+		MonsterInstance.CurrentHp = level * data.HpPerLevel;
+		MonsterInstance.BasePAtk = level * data.PAtkPerLevel;
+		MonsterInstance.BaseMAtk = level * data.MAtkPerLevel;
+		MonsterInstance.BasePDef = level * data.PDefPerLevel;
+		MonsterInstance.BaseMDef = level * data.MDefPerLevel;
+		MonsterInstance.KarmaType = data.KarmaType;
+	}
+
+	private void SyncExportedMonsterFields()
+	{
+		if (MonsterInstance is null)
+		{
+			return;
+		}
+
+		var instance = MonsterInstance;
+		var data = instance.MonsterDataOrigin;
+
+		_level = instance.Level;
+		_isNamed = instance.IsNamed;
+		_currentHp = instance.CurrentHp;
+
+		_maxHp = instance.MaxHp;
+		_basePAtk = instance.BasePAtk;
+		_baseMAtk = instance.BaseMAtk;
+		_basePDef = instance.BasePDef;
+		_baseMDef = instance.BaseMDef;
+		_instanceKarmaType = instance.KarmaType;
+
+		_dataGameId = data.GameId;
+		_dataObjectType = data.ObjectType;
+		_dataObjectKind = data.ObjectKind;
+		_dataSphereType = data.SphereType;
+		_dataModelNameGround = data.ModelNameGround;
+		_dataModelNameInventory = data.ModelNameInventory;
+		_dataKarmaType = data.KarmaType;
+		_dataMinLevel = data.MinLevel;
+		_dataMaxLevel = data.MaxLevel;
+		_dataHpPerLevel = data.HpPerLevel;
+		_dataPDefPerLevel = data.PDefPerLevel;
+		_dataMDefPerLevel = data.MDefPerLevel;
+		_dataPAtkPerLevel = data.PAtkPerLevel;
+		_dataMAtkPerLevel = data.MAtkPerLevel;
+		_dataMutatorId = data.MutatorId;
+		_dataMutatorName = data.MutatorName;
+		_dataSpeed = data.Speed;
+		_dataRange = data.Range;
+		_dataAttackDelay = data.AttackDelay;
+	}
+
+	private void NotifyInspector()
+	{
+		if (Engine.IsEditorHint())
+		{
+			NotifyPropertyListChanged();
+		}
+	}
+
 	protected override string ResolveModelNameFromObjectTypeFallback()
 	{
 		if (TryGetMonsterModelNameGroundFromDb(MonsterType, out var ground))
@@ -57,13 +143,326 @@ public partial class Monster : WorldObject
 			}
 
 			_monsterType = value;
+			RefreshMonsterInstanceFromType();
 			if (IsInsideTree())
 			{
 				CallDeferred(nameof(RefreshModelVisualDeferred));
 			}
 		}
 	}
-	[Export] public bool HasName { get; set; }
+
+	[ExportGroup("Monster Instance")]
+	private int _level = 1;
+
+	[Export]
+	public int Level
+	{
+		get => _level;
+		set
+		{
+			if (_level == value)
+			{
+				return;
+			}
+
+			_level = value;
+			if (MonsterInstance is not null)
+			{
+				MonsterInstance.Level = value;
+				RecalculateMonsterInstanceStatsFromLevel();
+				SyncExportedMonsterFields();
+			}
+
+			NotifyInspector();
+		}
+	}
+
+	private bool _isNamed;
+
+	[Export]
+	public bool IsNamed
+	{
+		get => _isNamed;
+		set
+		{
+			if (_isNamed == value)
+			{
+				return;
+			}
+
+			_isNamed = value;
+			if (MonsterInstance is not null)
+			{
+				MonsterInstance.IsNamed = value;
+			}
+
+			NotifyInspector();
+		}
+	}
+
+	private int _currentHp;
+
+	[Export]
+	public int CurrentHp
+	{
+		get => _currentHp;
+		set
+		{
+			if (_currentHp == value)
+			{
+				return;
+			}
+
+			_currentHp = value;
+			if (MonsterInstance is not null)
+			{
+				MonsterInstance.CurrentHp = value;
+			}
+
+			NotifyInspector();
+		}
+	}
+
+	private int _maxHp;
+
+	[Export]
+	public int MaxHp
+	{
+		get => _maxHp;
+		private set => _maxHp = value;
+	}
+
+	private int _basePAtk;
+
+	[Export]
+	public int BasePAtk
+	{
+		get => _basePAtk;
+		private set => _basePAtk = value;
+	}
+
+	private int _baseMAtk;
+
+	[Export]
+	public int BaseMAtk
+	{
+		get => _baseMAtk;
+		private set => _baseMAtk = value;
+	}
+
+	private int _basePDef;
+
+	[Export]
+	public int BasePDef
+	{
+		get => _basePDef;
+		private set => _basePDef = value;
+	}
+
+	private int _baseMDef;
+
+	[Export]
+	public int BaseMDef
+	{
+		get => _baseMDef;
+		private set => _baseMDef = value;
+	}
+
+	private KarmaTypes _instanceKarmaType;
+
+	[Export]
+	public KarmaTypes InstanceKarmaType
+	{
+		get => _instanceKarmaType;
+		private set => _instanceKarmaType = value;
+	}
+
+	[ExportGroup("Monster Data")]
+	private int _dataGameId;
+
+	[Export]
+	public int DataGameId
+	{
+		get => _dataGameId;
+		private set => _dataGameId = value;
+	}
+
+	private GameObjectType _dataObjectType;
+
+	[Export]
+	public GameObjectType DataObjectType
+	{
+		get => _dataObjectType;
+		private set => _dataObjectType = value;
+	}
+
+	private GameObjectKind _dataObjectKind;
+
+	[Export]
+	public GameObjectKind DataObjectKind
+	{
+		get => _dataObjectKind;
+		private set => _dataObjectKind = value;
+	}
+
+	private string _dataSphereType = string.Empty;
+
+	[Export]
+	public string DataSphereType
+	{
+		get => _dataSphereType;
+		private set => _dataSphereType = value;
+	}
+
+	private string _dataModelNameGround = string.Empty;
+
+	[Export]
+	public string DataModelNameGround
+	{
+		get => _dataModelNameGround;
+		private set => _dataModelNameGround = value;
+	}
+
+	private string _dataModelNameInventory = string.Empty;
+
+	[Export]
+	public string DataModelNameInventory
+	{
+		get => _dataModelNameInventory;
+		private set => _dataModelNameInventory = value;
+	}
+
+	private KarmaTypes _dataKarmaType;
+
+	[Export]
+	public KarmaTypes DataKarmaType
+	{
+		get => _dataKarmaType;
+		private set => _dataKarmaType = value;
+	}
+
+	private int _dataMinLevel;
+
+	[Export]
+	public int DataMinLevel
+	{
+		get => _dataMinLevel;
+		private set => _dataMinLevel = value;
+	}
+
+	private int _dataMaxLevel;
+
+	[Export]
+	public int DataMaxLevel
+	{
+		get => _dataMaxLevel;
+		private set => _dataMaxLevel = value;
+	}
+
+	private int _dataHpPerLevel;
+
+	[Export]
+	public int DataHpPerLevel
+	{
+		get => _dataHpPerLevel;
+		private set => _dataHpPerLevel = value;
+	}
+
+	private int _dataPDefPerLevel;
+
+	[Export]
+	public int DataPDefPerLevel
+	{
+		get => _dataPDefPerLevel;
+		private set => _dataPDefPerLevel = value;
+	}
+
+	private int _dataMDefPerLevel;
+
+	[Export]
+	public int DataMDefPerLevel
+	{
+		get => _dataMDefPerLevel;
+		private set => _dataMDefPerLevel = value;
+	}
+
+	private int _dataPAtkPerLevel;
+
+	[Export]
+	public int DataPAtkPerLevel
+	{
+		get => _dataPAtkPerLevel;
+		private set => _dataPAtkPerLevel = value;
+	}
+
+	private int _dataMAtkPerLevel;
+
+	[Export]
+	public int DataMAtkPerLevel
+	{
+		get => _dataMAtkPerLevel;
+		private set => _dataMAtkPerLevel = value;
+	}
+
+	private int _dataMutatorId;
+
+	[Export]
+	public int DataMutatorId
+	{
+		get => _dataMutatorId;
+		private set => _dataMutatorId = value;
+	}
+
+	private string _dataMutatorName = string.Empty;
+
+	[Export]
+	public string DataMutatorName
+	{
+		get => _dataMutatorName;
+		private set => _dataMutatorName = value;
+	}
+
+	private float _dataSpeed;
+
+	[Export]
+	public float DataSpeed
+	{
+		get => _dataSpeed;
+		private set => _dataSpeed = value;
+	}
+
+	private int _dataRange;
+
+	[Export]
+	public int DataRange
+	{
+		get => _dataRange;
+		private set => _dataRange = value;
+	}
+
+	private float _dataAttackDelay;
+
+	[Export]
+	public float DataAttackDelay
+	{
+		get => _dataAttackDelay;
+		private set => _dataAttackDelay = value;
+	}
+
+	public override void _Ready()
+	{
+		if (MonsterInstance is null)
+		{
+			RefreshMonsterInstanceFromType();
+		}
+		else
+		{
+			SyncExportedMonsterFields();
+		}
+
+		base._Ready();
+	}
+
 	[Export] public int NameID_1 { get; set; }
 	[Export] public int NameID_2 { get; set; }
 	[Export] public int NameID_3 { get; set; }
@@ -71,8 +470,6 @@ public partial class Monster : WorldObject
 
 	protected override List<PacketPart> GetPacketParts()
 	{
-		// TODO some cats are placed by hand and got no MonsterInstance
-		// TODO named and higher levels
 		return (MonsterInstance?.Level ?? 1) - 1 == 0
 			? PacketPart.LoadDefinedWithOverride("monster_level_1")
 			: PacketPart.LoadDefinedWithOverride("monster_full");
@@ -100,16 +497,12 @@ public partial class Monster : WorldObject
 		var mobTypeId = MonsterTypeMapping.MonsterNameToMonsterTypeMapping[MonsterType];
 		PacketPart.UpdateValue(packetParts, "mob_type", mobTypeId, 14);
 		var levelToEncode = (MonsterInstance?.Level ?? 1) - 1;
-		// TODO level without these
 		if (levelToEncode > 0)
 		{
-			// var levelValue = (0b0001000 << 22) + (((Level >> 5) & 0b11111) << 17) + (0b101001111101 << 5) +
-			//                  (Level & 0b11111);
-			// PacketPart.UpdateValue(packetParts, "level", levelValue, 29);
 			PacketPart.UpdateValue(packetParts, "level_last_3", levelToEncode & 0b111, 3);
 		}
 
-		if (HasName)
+		if (IsNamed)
 		{
 		}
 
